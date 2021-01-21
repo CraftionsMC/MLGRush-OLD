d762410912886a8289ad36691207b475865ce280
MCTzOCK <dev@mctzock.de>
2021-01-21 18:55:36 +0100

    Added Final Pitch

---
+++ src/main/java/net/craftions/mlg/events/EventPlayerQuit.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021 Ben Siebert. All rights reserved.
+ */
+package net.craftions.mlg.events;
+
+import net.craftions.mlg.Mlg;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class EventPlayerQuit implements Listener {
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e){
+        if(e.getPlayer().equals(Mlg.player1) || e.getPlayer().equals(Mlg.player2)){
+            for(Player p : Bukkit.getOnlinePlayers()){
+                p.kickPlayer("Ein Spieler hat die Runde verlassen!");
+            }
+        }
+    }
+
+}
